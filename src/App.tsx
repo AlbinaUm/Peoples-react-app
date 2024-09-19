4578f65b7a23321ca50d74cec34eea15dbf58415
Umarova Albina <albinaumarova2014@gmail.com>
2024-09-19 19:55:02 +0600

    Lesson-54 Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,113 +1,82 @@
 import "./App.css";
-import { useState } from "react";
-import Person from "./components/Person/Person.tsx";
+import {useState} from "react";
+import People from "./components/People/People.tsx";
+import {IPerson} from "./types";
+import Counter from "./components/Counter/Counter.tsx";
+import ToggleBtn from "./components/ToggleBtn/ToggleBtn.tsx";
 
-interface IPerson {
-  name: string;
-  age: number;
-  hobby?: string | null;
-  id: number;
-}
 
 const App = () => {
-  const [people, setPeople] = useState<IPerson[]>([
-    { id: 1, name: "John", age: 28, hobby: "Coding" },
-    { id: 2, name: "Jane", age: 30, hobby: "Swimming" },
-    { id: 3, name: "Lena", age: 10, hobby: "Draw" },
-    { id: 4, name: "Vasya", age: 50, hobby: "Coding" },
-  ]);
-  const [togglePeople, setTogglePeople] = useState(true);
+    const [people, setPeople] = useState<IPerson[]>([
+        {id: 1, name: "John", age: 28, hobby: "Coding"},
+        {id: 2, name: "Jane", age: 30, hobby: "Swimming"},
+        {id: 3, name: "Lena", age: 10, hobby: "Draw"},
+        {id: 4, name: "Vasya", age: 50, hobby: "Coding"},
+    ]);
+    const [togglePeople, setTogglePeople] = useState<boolean>(true);
 
-  const changeNameToFirstPerson = () => {
-    console.log("Click the button");
-    const copyPeople = [...people];
-    const copyPerson = { ...copyPeople[0] };
-    copyPerson.name = "Albina";
+    const changeName = (e: React.ChangeEvent<HTMLInputElement>, id: number) => {
 
-    copyPeople[0] = copyPerson;
+        const copyPeople = people.map((person) => {
+            if (person.id === id) {
+                return {
+                    ...person,
+                    name: e.target.value,
+                };
+            }
+            return {...person};
+        });
 
-    setPeople(copyPeople);
-  };
+        setPeople(copyPeople);
+    };
 
-  const changeName = (e: React.ChangeEvent<HTMLInputElement>, id: number) => {
+    const changeAge = (id: number) => {
+        const index = people.findIndex((person) => person.id === id);
 
-    const copyPeople = people.map((person, i) => {
-      if (person.id === id) {
-        return {
-          ...person,
-          name: e.target.value,
-        };
-      }
-      return { ...person };
-    });
+        const copyPeople = people.map((person, i) => {
+            if (i === index) {
+                return {
+                    ...person,
+                    age: person.age + 1,
+                };
+            }
+            return {...person};
+        });
 
-    setPeople(copyPeople);
-  };
+        setPeople(copyPeople);
+    };
 
-  const changeAge = (id: number) => {
-    let index = people.findIndex((person) => person.id === id);
+    let peopleList = null;
 
-    const copyPeople = people.map((person, i) => {
-      if (i === index) {
-        return {
-          ...person,
-          age: person.age + 1,
-        };
-      }
-      return { ...person };
-    });
+    const onTooglePeople = () => {
+        setTogglePeople((prevState) => !prevState);
+    };
 
-    setPeople(copyPeople);
-  };
+    const deletePerson = (id: number) => {
+        const copyPeople = people.filter((person) => person.id !== id);
 
-  let peopleList = null;
+        setPeople(copyPeople);
+    };
 
-  const onTooglePeople = () => {
-    setTogglePeople((prevState) => !prevState);
-  };
+    if (togglePeople) {
+        peopleList = (
+            <People
+                people={people}
+                deletePerson={deletePerson}
+                changeName={changeName}
+                changeAge={changeAge}
+            />
+        );
+    }
 
-  const deletePerson = (id: number) => {
-    const copyPeople = people.filter((person) => person.id !== id);
+    return (
+        <div className="App">
+            {peopleList}
 
-    setPeople(copyPeople);
-  };
-
-  if (togglePeople) {
-    peopleList = (
-      <div>
-        {people.map((person) => (
-          <Person
-            key={person.id}
-            name={person.name}
-            age={person.age}
-            onChangeAge={() => changeAge(person.id)}
-            onChangeName={(e) => changeName(e, person.id)}
-            onDeletePersonById={() => deletePerson(person.id)}
-          >
-            {person.hobby ? (
-              <p>
-                <b>Hobby:</b> {person.hobby}
-              </p>
-            ) : null}
-          </Person>
-        ))}
-      </div>
+            <ToggleBtn onTooglePeople={onTooglePeople} togglePeople={togglePeople}/>
+            <Counter people={people}/>
+        </div>
     );
-  }
-
-  return (
-    <>
-      {peopleList}
-
-      <button onClick={changeNameToFirstPerson} type="button">
-        Change name to first person
-      </button>
-
-      <button type="button" onClick={onTooglePeople}>
-        {togglePeople ? "Hide people" : "Show people"}
-      </button>
-    </>
-  );
 };
 
 export default App;
